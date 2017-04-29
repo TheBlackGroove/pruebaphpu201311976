a94d9261ab69ae2e0cf11e5694f7bdf6b6fade0b
Fabio Catriel Cruz Gonella <u201311976@upc.edu.pe>
2017-04-29 21:18:06 +0000

    implementacion de bd

--- index.php
+++ index.php
@@ -9,21 +9,34 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
  	</head>
  <body>
-
-	<?php
-	$servername = "us-cdbr-iron-east-03.cleardb.net";
-	$username = "b74ba3320e82ec";
-	$password = "2d194843";
-	$dbname = "ad_bb6ddb2f34daf52";
-	
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-	$sql = "SELECT * from empleados";
-	$result = $conn->query($sql);
-	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {?>
+	<table>
+		<?php
+		$servername = "us-cdbr-iron-east-03.cleardb.net";
+		$username = "b74ba3320e82ec";
+		$password = "2d194843";
+		$dbname = "ad_bb6ddb2f34daf52";
+		
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		$sql = "SELECT * from servicios";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {?>
+			<?php echo "<tr>"; ?>
+			<?php echo "<td>", $row['codigo_servicio'], "</td>"; ?>
+			<?php echo "<td>", $row['nombre_servicio'], "</td>"; ?>
+			<?php echo "<td>", $row['descripcion_servicio'], "</td>"; ?>
+			<?php echo "<td>", $row['imegen_servicio'], "</td>"; ?>
+			<?php echo "</tr>"; ?>
+			<?php }
+				} 
+				else {
+					echo "0 results";
+					  }
+				$conn->close();?> 
+	</table>
  </body>
  </html>
