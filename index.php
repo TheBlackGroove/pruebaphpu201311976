c4b1da5d1d6bf883803abdf51c72398fbd3a3ead
Fabio Catriel Cruz Gonella <u201311976@upc.edu.pe>
2017-04-29 21:54:21 +0000

    Avances Realizados

--- index.php
+++ index.php
@@ -9,34 +9,54 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
  	</head>
  <body>
-	<table>
-		<?php
-		$servername = "us-cdbr-iron-east-03.cleardb.net";
-		$username = "b74ba3320e82ec";
-		$password = "2d194843";
-		$dbname = "ad_bb6ddb2f34daf52";
-		
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}
-		$sql = "SELECT * from servicios";
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-			while($row = $result->fetch_assoc()) {?>
-			<?php echo "<tr>"; ?>
-			<?php echo "<td>", $row['codigo_servicio'], "</td>"; ?>
-			<?php echo "<td>", $row['nombre_servicio'], "</td>"; ?>
-			<?php echo "<td>", $row['descripcion_servicio'], "</td>"; ?>
-			<?php echo "<td>", $row['imegen_servicio'], "</td>"; ?>
-			<?php echo "</tr>"; ?>
-			<?php }
-				} 
-				else {
-					echo "0 results";
-					  }
-				$conn->close();?> 
-	</table>
+ 	<div class="container-fluid div-titulo" align="center">
+ 		<h1>Prueba</h1>
+ 		<h3>Alumno: Fabio Catriel Cruz Gonella</h3>
+ 	</div>
+ 	<div class="container" style="margin-top: 100px;" align="center">
+ 		<img src="../images/header.PNG"/>
+ 	</div>
+ 	<div class="container" align="center" >
+		<table>
+			<theader>
+				<td>icono</td>
+				<td>Codigo</td>
+				<td>Nombre</td>
+				<td>Descripcion</td>
+			</theader>
+			<tbody>
+				<?php
+				$servername = "us-cdbr-iron-east-03.cleardb.net";
+				$username = "b74ba3320e82ec";
+				$password = "2d194843";
+				$dbname = "ad_bb6ddb2f34daf52";
+				
+				$conn = new mysqli($servername, $username, $password, $dbname);
+				
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				}
+				$sql = "SELECT * from servicios";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()) {?>
+					<?php echo "<tr>"; ?>				
+					<?php echo "<td>", $row['imegen_servicio'], "</td>"; ?>
+					<?php echo "<td>", $row['codigo_servicio'], "</td>"; ?>
+					<?php echo "<td>", $row['nombre_servicio'], "</td>"; ?>
+					<?php echo "<td>", $row['descripcion_servicio'], "</td>"; ?>
+					<?php echo "</tr>"; ?>
+					<?php }
+						} 
+						else {
+							echo "0 results";
+							  }
+						$conn->close();?> 
+			</tbody>
+		</table>
+	</div>
+	<div class="container" align="center">
+		<img src="../images/footer.PNG"/>
+	</div>
  </body>
  </html>
